946d5cc57874ed6f22fcfc5126bc8c3b0d492d7b
Erfan <eftekharierfan5555555555@gmail.com>
2024-04-28 20:31:10 +0330

    update _document file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,25 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import "./globals.css";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="fa">
       <Head />
-      <body>
+      <body className={inter.className}>
         <Main />
         <NextScript />
       </body>
     </Html>
   );
 }
+
+export const metadata = {
+  title: "صفحه اصلی - SET Coffee | فروشگاه اینترنتی قهوه ست",
+  description: "Sabzlearn coffee project with next.js v13",
+
+  icons: {
+    icon: "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/36190/coffee-logo-clipart-md.png",
+  },
+};
